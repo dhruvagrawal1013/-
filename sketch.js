dc6d3a64ea330ca4ec70edf492f5ff1c96e28f5e
dhruvagrawal1013 <88618856+dhruvagrawal1013@users.noreply.github.com>
2021-11-25 14:08:17 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,40 @@
+var bg1,bg2;
+
+
+
+function preload(){
+bg1img=loadImage("background2.jpg")
+}
+
+function setup() {
+ createCanvas(windowWidth,windowHeight)
+ space = createSprite(windowWidth,windowHeight)
+ space. addImage(bg1img)
+}
+
+function draw() {
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+    drawSprites();
+
+
+}
